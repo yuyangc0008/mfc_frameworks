8bdd2a4d626dae2fcbdc22bdc9febe02a81de0be
yuyang <yuyangc0008@163.com>
2018-04-22 15:59:53 +0800

    mfc dialog base class

---
+++ dialogs/MfcDialogBase.cpp
@@ -0,0 +1,176 @@
+#include "stdafx.h"
+#include "MfcDialogBase.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+CMfcDialogBase::CMfcDialogBase(UINT nIDIcon, UINT nIDTemplate, CWnd* pParent/* = NULL*/)
+	: CDialog(nIDTemplate, pParent)
+	, m_DlgRect(0, 0, 400, 100)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(nIDIcon);
+}
+
+CMfcDialogBase::~CMfcDialogBase()
+{
+	RemoveAllCtrls();
+}
+
+void CMfcDialogBase::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMfcDialogBase, CDialog)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+
+BOOL CMfcDialogBase::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+	InitDlgFont();
+	InitCtrlsProp();
+	CreateCtrls();
+	SetCtrlsLayout();
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CMfcDialogBase::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialog::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CMfcDialogBase::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+void CMfcDialogBase::InitDlgFont()
+{
+	LOGFONT lf;
+	memset(&lf, 0, sizeof(LOGFONT));
+	lf.lfHeight = -16;
+#ifdef _UNICODE
+	swprintf_s(lf.lfFaceName, 32, L"Arial");
+#else
+	sprintf_s(lf.lfFaceName, 32, "Arial");
+#endif
+	m_DlgFont.CreateFontIndirect(&lf);
+}
+
+void CMfcDialogBase::InitCtrlsProp()
+{
+#ifdef _DEBUG
+	m_DlgCtrlsProp.RemoveAll();
+	AddCtrlProp(CTRL_LABEL, CRect(0, 0, m_DlgRect.Width(), m_DlgRect.Height()),
+		0, _T("Static"), _T("CMfcDialogBase::InitCtrlsProp() must be overwritten"),
+		WS_CHILD|WS_VISIBLE|SS_LEFT, this, 0xffffff, NULL, &m_DlgFont);
+#endif
+}
+
+void CMfcDialogBase::CreateCtrls()
+{
+	RemoveAllCtrls();
+	for (INT_PTR i = 0; i < m_DlgCtrlsProp.GetCount(); i++)
+	{
+		sCtrlProp &prop = m_DlgCtrlsProp[i];
+		CWnd *wnd = new CWnd();
+		wnd->CreateEx(prop.dwExStyle, prop.lpszClassName, prop.lpszWindowName,
+			prop.dwStyle, prop.rcPos, prop.pParent, prop.nID, prop.lpParam);
+		wnd->SetFont(prop.pFont);
+		m_DlgCtrls.Add(wnd);
+	}
+	ASSERT(m_DlgCtrlsProp.GetCount() == m_DlgCtrls.GetCount());
+}
+
+void CMfcDialogBase::SetCtrlsLayout()
+{
+	SetWindowPos(NULL, 0, 0, m_DlgRect.Width(), m_DlgRect.Height(),
+		SWP_NOMOVE);
+	ASSERT(m_DlgCtrlsProp.GetCount() == m_DlgCtrls.GetCount());
+	for (INT_PTR i = 0; i < m_DlgCtrlsProp.GetCount(); i++)
+		m_DlgCtrls[i]->MoveWindow(m_DlgCtrlsProp[i].rcPos);
+}
+
+void CMfcDialogBase::AddCtrlProp(const sCtrlProp& prop)
+{
+	m_DlgCtrlsProp.Add(prop);
+}
+
+void CMfcDialogBase::AddCtrlProp(eCtrlType nType, const CRect& rcPos, DWORD dwExStyle,
+	LPCTSTR lpszClassName, LPCTSTR lpszWindowName, DWORD dwStyle,
+	CWnd* pParent, UINT nID, LPVOID lpParam, CFont* pFont)
+{
+	sCtrlProp prop;
+	prop.nType = nType;
+	prop.rcPos = rcPos;
+	prop.dwExStyle = dwExStyle;
+	prop.lpszClassName = lpszClassName;
+	prop.lpszWindowName = lpszWindowName;
+	prop.dwStyle = dwStyle;
+	prop.pParent = pParent;
+	prop.nID = nID;
+	prop.lpParam = lpParam;
+	prop.pFont = pFont;
+	AddCtrlProp(prop);
+}
+
+void CMfcDialogBase::AddCtrlPropCustom(const CRect& rcPos, CWnd* pCustomWnd)
+{
+	if (pCustomWnd)
+	{
+		sCtrlProp prop;
+		prop.nType = CTRL_CUSTOM;
+		prop.rcPos = rcPos;
+		prop.pCustomWnd = pCustomWnd;
+		AddCtrlProp(prop);
+	}
+}
+
+void CMfcDialogBase::RemoveAllCtrls()
+{
+	for (INT_PTR i = 0; i < m_DlgCtrls.GetCount(); i++)
+	{
+		m_DlgCtrls[i]->DestroyWindow();
+		delete m_DlgCtrls[i];
+	}
+	m_DlgCtrls.RemoveAll();
+}
+
---
+++ dialogs/MfcDialogBase.h
@@ -0,0 +1,121 @@
+#ifndef _MFC_DIALOG_BASE_H_V100_YY_
+#define _MFC_DIALOG_BASE_H_V100_YY_
+
+/*
+ * MFC Dialog basic class
+ *@author: clarkyy
+ *@date: 20180414
+ *@usage:
+ *  child class must overwrite InitCtrlsProp(), others are not necessary.
+ *  /// test.h
+ *  class CMFCDlgTest : public CMfcDialogBase
+ *  {
+ *  // constructor
+ *  public:
+ *  	CMFCDlgTest(CWnd* pParent = NULL);
+ *  // windows message/event handle
+ *  protected:
+ *  	afx_msg void OnBtnClkTest();
+ *  	DECLARE_MESSAGE_MAP()
+ *  // functions to be overwritten 
+ *  protected:
+ *  	virtual void InitCtrlsProp();
+ *  };
+ *
+ *  /// test.cpp
+ *  CMFCDlgTest::CMFCDlgTest(CWnd* pParent)
+ *  : CMfcDialogBase(IDR_MAINFRAME, IDD_MFC000_DIALOG, pParent)
+ *  {
+ *  }
+ *  
+ *  BEGIN_MESSAGE_MAP(CMFCDlgTest, CMfcDialogBase)
+ *  	ON_BN_CLICKED(IDC_BTN_TEST, &OnBtnClkTest)
+ *  END_MESSAGE_MAP()
+ *  
+ *  void CMFCDlgTest::InitCtrlsProp()
+ *  {
+ *  	m_DlgRect.SetRect(0, 0, 400, 400);
+ *  	m_DlgCtrlsProp.RemoveAll();
+ *  	AddCtrlProp(CTRL_LABEL, CRect(0, 0, m_DlgRect.Width(), 20),
+ *  		0, _T("STATIC"), _T("Label Test"),
+ *  		WS_CHILD | WS_VISIBLE | SS_LEFT, this, 0xffffff, NULL, &m_DlgFont);
+ *  	AddCtrlProp(CTRL_BUTTON, CRect(0, 70, m_DlgRect.Width(), 100),
+ *  		0, _T("BUTTON"), _T("Button Test"),
+ *  		WS_CHILD | WS_VISIBLE | BS_CENTER, this, IDC_BTN_TEST, NULL, &m_DlgFont);
+ *..	// more...
+ *  }
+ *  
+ *  void CMFCDlgTest::OnBtnClkTest()
+ *  {
+ *  	MessageBox(_T("test done"), _T("Button Test"), MB_OK);
+ *  }
+ *
+ *
+ */
+
+typedef enum eCtrlType
+{
+	CTRL_LABEL = 0,          // CStatic
+	CTRL_BUTTON,             // CButton
+	CTRL_EDIT,               // CEdit
+	CTRL_COMBOBOX,           // CComboBox
+	CTRL_LISTBOX,            // CListBox
+	CTRL_CUSTOM,             // custom
+	CTRL_COUNT
+} eCtrlType;
+
+typedef struct sCtrlProp
+{
+	eCtrlType      nType;
+	CRect          rcPos;
+	DWORD          dwExStyle;
+	LPCTSTR        lpszClassName;
+	LPCTSTR        lpszWindowName;
+	DWORD          dwStyle;
+	CWnd*          pParent;
+	UINT           nID;
+	LPVOID         lpParam;
+	CFont*         pFont;
+	CWnd*          pCustomWnd;
+	sCtrlProp()
+	{
+		memset(this, 0, sizeof(sCtrlProp));
+	}
+} sCtrlProp;
+
+class CMfcDialogBase : public CDialog
+{
+public:
+	CMfcDialogBase(UINT nIDIcon, UINT nIDTemplate, CWnd* pParent = NULL);
+	virtual ~CMfcDialogBase();
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);
+
+protected:
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+protected:
+	void AddCtrlProp(const sCtrlProp& prop);
+	void AddCtrlProp(eCtrlType nType, const CRect& rcPos, DWORD dwExStyle,
+		LPCTSTR lpszClassName, LPCTSTR lpszWindowName, DWORD dwStyle,
+		CWnd* pParent, UINT nID, LPVOID lpParam, CFont* pFont);
+	void AddCtrlPropCustom(const CRect& rcPos, CWnd* pCustomWnd);
+	void RemoveAllCtrls();
+	virtual void InitDlgFont();
+	virtual void InitCtrlsProp();
+	virtual void CreateCtrls();
+	virtual void SetCtrlsLayout();
+
+protected:
+	HICON                    m_hIcon;
+	CFont                    m_DlgFont;
+	CArray<sCtrlProp>        m_DlgCtrlsProp;
+	CArray<CWnd*>            m_DlgCtrls;
+	CRect                    m_DlgRect;
+};
+
+#endif /* _MFC_DIALOG_BASE_H_V100_YY_ */
